394408516a73430ae40f5181b243fa4fad38dfb8
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2025-05-01 20:30:06 +0200

    Upgrade binutils 2.44

--- config/ps2toolchain-iop-config.sh
+++ config/ps2toolchain-iop-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PS2TOOLCHAIN_IOP_BINUTILS_REPO_URL="https://github.com/bminor/binutils-gdb.git"
-PS2TOOLCHAIN_IOP_BINUTILS_DEFAULT_REPO_REF="binutils-2_43_1"
+PS2TOOLCHAIN_IOP_BINUTILS_REPO_URL="https://github.com/ps2dev/binutils-gdb.git"
+PS2TOOLCHAIN_IOP_BINUTILS_DEFAULT_REPO_REF="iop-v2.44.0"
 PS2TOOLCHAIN_IOP_GCC_REPO_URL="https://github.com/gcc-mirror/gcc.git"
 PS2TOOLCHAIN_IOP_GCC_DEFAULT_REPO_REF="releases/gcc-14.2.0"
 
