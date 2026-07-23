d7ead3c9f3d3220b3ca0b17641fd5b7fe3d734cf
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2026-07-23 19:00:43 +0200

    Use gnu.googlesource mirror for binutils

--- config/ps2toolchain-iop-config.sh
+++ config/ps2toolchain-iop-config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-PS2TOOLCHAIN_IOP_BINUTILS_REPO_URL="https://sourceware.org/git/binutils-gdb.git"
+# Using gnu.googlesource.com instead of sourceware.org because the clone is producing a 
+# RPC failed; HTTP 502 curl 22 The requested URL returned error: 502
+PS2TOOLCHAIN_IOP_BINUTILS_REPO_URL="https://gnu.googlesource.com/binutils-gdb"
 PS2TOOLCHAIN_IOP_BINUTILS_DEFAULT_REPO_REF="binutils-2_45_1"
-PS2TOOLCHAIN_IOP_GCC_REPO_URL="https://gcc.gnu.org/git/gcc.git"
+PS2TOOLCHAIN_IOP_GCC_REPO_URL="https://gnu.googlesource.com/gcc"
 PS2TOOLCHAIN_IOP_GCC_DEFAULT_REPO_REF="releases/gcc-15.2.0"
 
 if test -f "$PS2DEV_CONFIG_OVERRIDE"; then
